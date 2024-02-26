48abfa017476a04f69b9baa3db7ee6ae6d2222ef
Axel Ha <axelensiie@gmail.com>
2024-02-25 21:40:24 -0500

    Update from FIGMA

--- src/components/SubscribeButton.jsx
+++ src/components/SubscribeButton.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 
 const SubscribeButton = ({ rightChevronImageUrl, blueArrowImageUrl }) => {
-  const CONVERTKIT_FORM_IF = 6188862;
   const [email, setEmail] = useState("");
 
   const handleSubmit = async (e) => {
@@ -36,26 +35,28 @@ const SubscribeButton = ({ rightChevronImageUrl, blueArrowImageUrl }) => {
   return (
     <div className={"cta"}>
       <div className={"cta-business"}>
-        <p className={"cta-business-title"}>TU VEUX NOUS REJOINDRE ?</p>
-        <p className={"cta-business-description"}>Tous les Dimanche, on navigue dans l'incertitude pour apprendre à Discerner & Décider.</p>
+        <p className={"cta-business-title"}>Comment provoquer la chance ? </p>
+        <p className={"cta-business-description"}>
+          Difficile de naviguer sur les eaux incertaines du business... Découvrez comment affiner votre processus décisionnel pour marcher vers le
+          succès.
+        </p>
       </div>
       <div className="arrow-delimiter">
         <img src={blueArrowImageUrl} alt="-->" width={50} height={50} />
       </div>
       <div className={"cta-start"}>
-        <p className={"cta-business-title"}>COMMENCE ICI</p>
+        <p className={"cta-business-title"}>Commencez ici.</p>
         <input
           name="email_address"
           aria-label="E-mail"
-          placeholder="Inscris ton adresse mail..."
+          placeholder="Inscrivez votre adresse mail..."
           value={email}
           onChange={(e) => setEmail(e.target.value)}
         />
       </div>
       <div className={"cta-submit"}>
         <button className={"btn"} type="submit" onClick={handleSubmit}>
-          Je découvre
-          <img src={rightChevronImageUrl} alt="-->" width={20} height={20} />
+          Accès Gratuit
         </button>
       </div>
     </div>
